fd6423f7d801f986607766a783ffc7337a0dc074
Zzzzzshan <zshanfarooq67@gmail.com>
2023-01-15 17:31:54 +0400

    Header embedded

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+const Header = () => {
+  return (
+    <div class="container">
+      <div class="wrapper_div">
+        <div class="logo_main">UNIVERSITY OF OULU</div>
+        <div class="nav">
+          <a href="#">Admissions</a>
+          <a href="#">Research</a>
+          <a href="#">Cooperation</a>
+          <a href="#">University</a>
+          <a href="#">For Students</a>
+          <a href="#">
+            Search<i class="fa fa-search"></i>{" "}
+          </a>
+          <a href="#">
+            EN<i class="fa fa-globe"></i>{" "}
+          </a>
+          <a href="#">
+            Menu<i class="fa fa-bars"></i>{" "}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
